49bc07a975b5180a9b2606710722a73a55aeff38
Joe Fitter <joefitter@hotmail.com>
2015-08-05 17:13:45 +0100

    test

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ import $ from 'jquery';
 import Backbone from 'backbone';
 import Marionette from 'backbone.marionette';
 
-const hi = 'hi';
+const Application = Marionette.Application.extend({
+  onStart() {
+    console.log('start');
+    console.log($);
+    console.log(Backbone.$);
+  }
+});
 
-export {hi};
+export {Application};
