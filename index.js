966c839f5cecbede68ffad8ef0c804cccb377906
Joe Fitter <joefitter@hotmail.com>
2015-08-06 14:44:04 +0100

    test

--- index.js
+++ index.js
@@ -6,17 +6,33 @@ import Backbone from 'backbone';
 import Cocktail from 'backbone.cocktail';
 import Radio from 'backbone.radio';
 import Handlebars from 'handlebars';
+import Base64 from './helpers/base64';
+import Collection from './mvc/collection';
+import Currency from './helpers/currency';
+import Device from 'browsernizr';
+import LocalStorage from './helpers/localStorage';
+import Translator from './helpers/translate';
+import Visibility from './helpers/visibility';
+import ModuleHelper from './helpers/module';
 import 'backbone.marionette';
+import 'backbone.stickit';
 
 const Orchestra = {};
 _.extend(Orchestra, Backbone);
-_.extend(Orchestra, Backbone.Marionette);
-
+_.extend(Orchestra, Backbone.Marionette.extend());
 
 export {
   $,
   _,
   Radio,
   Cocktail,
-  Orchestra
+  Orchestra,
+  Handlebars,
+  Base64,
+  Currency,
+  Device,
+  LocalStorage,
+  Translator,
+  Visibility,
+  ModuleHelper
 };
